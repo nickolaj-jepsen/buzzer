a9de898b623bdf48b50e20102fd83c5fdccf5f92
Nickolaj Jepsen <nickolaj@fireproof.website>
2025-07-03 23:59:33 +0200

    fix: get correct production url

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -2,11 +2,14 @@ export const baseUrl = () => {
   if (process.env.NEXT_PUBLIC_BASE_URL) {
     return process.env.NEXT_PUBLIC_BASE_URL;
   }
+  if (process.env.VERCEL_PROJECT_PRODUCTION_URL) {
+    return `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`;
+  }
+  if (process.env.VERCEL_BRANCH_URL) {
+    return `https://${process.env.VERCEL_BRANCH_URL}`;
+  }
   if (process.env.VERCEL_URL) {
     return `https://${process.env.VERCEL_URL}`;
-  }
-  if (typeof window !== "undefined") {
-    return window.location.origin;
   }
 
   return "http://localhost:3000";
